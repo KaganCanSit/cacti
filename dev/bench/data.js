df47dc885317d7460354a427d513f37f8961a8b4
github-action-benchmark <github@users.noreply.github.com>
2024-10-20 09:58:24 +0000

    add Benchmark (benchmarkjs) benchmark result for 927ddecd03d87d6cf5f222476c8762f123ac3022

---
+++ dev/bench/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1729418304273,
+  "repoUrl": "https://github.com/KaganCanSit/cacti",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "kagancansit@hotmail.com",
+            "name": "Kağan Can Şit",
+            "username": "KaganCanSit"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "927ddecd03d87d6cf5f222476c8762f123ac3022",
+          "message": "Merge branch 'hyperledger-cacti:main' into main",
+          "timestamp": "2024-10-20T12:41:18+03:00",
+          "tree_id": "5802f84f4083cdb62a4cb6a485110b28a97e5386",
+          "url": "https://github.com/KaganCanSit/cacti/commit/927ddecd03d87d6cf5f222476c8762f123ac3022"
+        },
+        "date": 1729418302502,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "cmd-api-server_HTTP_GET_getOpenApiSpecV1",
+            "value": 608,
+            "range": "±1.68%",
+            "unit": "ops/sec",
+            "extra": "179 samples"
+          },
+          {
+            "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
+            "value": 729,
+            "range": "±2.71%",
+            "unit": "ops/sec",
+            "extra": "183 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
